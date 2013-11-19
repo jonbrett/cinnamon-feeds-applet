a952f219234efd18fb7fa5cba4b1673b54761564
Niklas Meinzer <lamouletrescool@gmail.com>
2013-11-19 18:28:16 +0100

    App can now read feeds file and put the data into the table.

--- management app/manage_feeds.py
+++ management app/manage_feeds.py
@@ -7,12 +7,14 @@ from PyQt4.QtGui import (
     QApplication,
     QTableWidget,
     QTableWidgetItem,
-    QGridLayout,
     QWidget,
     QVBoxLayout,
     QHBoxLayout,
-    QPushButton
+    QPushButton,
+    QDesktopWidget,
+    QHeaderView
 )
+
 
 class MainWindow(QWidget):
 
@@ -21,17 +23,18 @@ class MainWindow(QWidget):
 
         self.setWindowTitle("Manage your RSS feeds")
 
-        self.table = QTableWidget(5,2)
+        self.table = QTableWidget(5, 2)
         self.table.setHorizontalHeaderLabels(["URL", "custom title"])
         self.table.verticalHeader().hide()
+        self.table.horizontalHeader().setResizeMode(0, QHeaderView.Stretch)
 
         # assemble GUI elements
         container = QVBoxLayout()
-        
+
         container.addWidget(self.table)
 
         buttons = QHBoxLayout()
-        
+
         self.cancel_button = QPushButton("Cancel")
         self.save_button = QPushButton("Save")
 
@@ -39,8 +42,63 @@ class MainWindow(QWidget):
         buttons.addWidget(self.cancel_button)
 
         container.addLayout(buttons)
-        
+
         self.setLayout(container)
+        self.resize(500, 300)
+        self.center()
+
+    def center(self):
+        """
+            Centers the window
+        """
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def fill_feed_list(self, feeds):
+        """
+            Takes a list of dicts as load_feed_file creates them
+            and fills the table widget with the content
+        """
+        for i, feed in enumerate(feeds):
+            self.table.setItem(i, 0, QTableWidgetItem(feed["url"]))
+            # if no custom title is set, display - None -
+            if feed["custom_title"] is None:
+                title = "- None -"
+            else:
+                title = feed["custom_title"]
+            self.table.setItem(i, 1, QTableWidgetItem(title))
+
+
+def load_feed_file(filename):
+    """
+        Reads content of the feed file and returns a list of
+        dicts {"url", "custom_title"}
+    """
+    content = []
+
+    with open(filename, "r") as f:
+        for line in f:
+            try:
+                if line[0] == "#":
+                    continue
+                temp = line.split()
+                url = temp[0]
+                custom_title = None
+                if len(temp) > 1:
+                    custom_title = " ".join(temp[1:])
+                content.append(
+                    {
+                        "url": url,
+                        "custom_title": custom_title
+                    }
+                )
+            except IndexError:
+                # empty lines are ignored
+                pass
+
+    return content
 
 if __name__ == '__main__':
 
@@ -49,9 +107,12 @@ if __name__ == '__main__':
 
     app = QApplication([])
 
+    feeds = load_feed_file(feed_file_name)
+
     app.window = MainWindow()
 
+    app.window.fill_feed_list(feeds)
+
     app.window.show()
-    
+
     sys.exit(app.exec_())
-    
