cfbc814b478926f0696c4464ad387ccf17fd888e
Jonathan Brett <jonbrett.dev@gmail.com>
2013-12-04 21:24:18 +0000

    Start port of feed management app to GTK

--- management app/manage_feeds.py
+++ management app/manage_feeds.py
@@ -1,213 +1,105 @@
+#!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 from __future__ import unicode_literals
-
-from functools import partial
+from gi.repository import Gtk
 
 import sys
 
-from PyQt4.QtGui import (
-    QApplication,
-    QTableWidget,
-    QTableWidgetItem,
-    QWidget,
-    QVBoxLayout,
-    QHBoxLayout,
-    QPushButton,
-    QDesktopWidget,
-    QHeaderView,
-    QCheckBox,
-)
+class MainWindow(Gtk.Window):
 
+    def __init__(self, filename):
+        super(Gtk.Window, self).__init__(title="Manage your feeds")
 
-class MainWindow(QWidget):
+        self.filename = filename;
+        self.feeds = self.load_feed_file(filename);
 
-    def __init__(self, feeds, app, filename):
-        super(MainWindow, self).__init__()
+        self.set_default_size(500, 200)
 
-        self.feed_list = feeds
+        self.treeview = Gtk.TreeView(model=self.feeds)
+        self.treeview.set_reorderable(True)
 
-        self.feed_file = filename
+        renderer_url= Gtk.CellRendererText()
+        renderer_url.set_property("editable", True)
+        renderer_url.connect("edited", self.url_edited)
+        column_url = Gtk.TreeViewColumn("Url", renderer_url, text=0)
+        column_url.set_expand(True)
+        self.treeview.append_column(column_url)
 
-        self.app = app
+        renderer_title= Gtk.CellRendererText()
+        renderer_title.set_property("editable", True)
+        renderer_title.connect("edited", self.title_edited)
+        column_title = Gtk.TreeViewColumn("Custom title", renderer_title, text=1)
+        column_title.set_expand(True)
+        self.treeview.append_column(column_title)
 
-        self.setWindowTitle("Manage your RSS feeds")
+        renderer_hidden = Gtk.CellRendererToggle()
+        renderer_hidden.connect("toggled", self.hidden_toggled)
+        column_hidden = Gtk.TreeViewColumn("Disabled", renderer_hidden, active=2)
+        column_hidden.set_expand(False)
+        self.treeview.append_column(column_hidden)
 
-        self.table = QTableWidget(1, 4)
-        self.table.setHorizontalHeaderLabels(
-            ["URL", "custom title", "hide", ""]
-        )
-        self.table.verticalHeader().hide()
-        self.table.horizontalHeader().setResizeMode(0, QHeaderView.Stretch)
-        self.table.cellChanged.connect(self.cell_changed)
+        self.add(self.treeview)
 
-        # assemble GUI elements
-        container = QVBoxLayout()
+    def url_edited(self, widget, path, text):
+        self.feeds[path][0] = text
 
-        container.addWidget(self.table)
-
-        self.addFeedButton = QPushButton("Add feed")
-        self.addFeedButton.clicked.connect(self.add_feed)
-        container.addWidget(self.addFeedButton)
-
-        buttons = QHBoxLayout()
-
-        self.cancel_button = QPushButton("Cancel")
-        self.cancel_button.clicked.connect(app.exit)
-        self.save_button = QPushButton("Save")
-
-        buttons.addWidget(self.save_button)
-        buttons.addWidget(self.cancel_button)
-
-        container.addLayout(buttons)
-
-        self.setLayout(container)
-        self.resize(500, 300)
-        self.center()
-
-        self.save_button.clicked.connect(self.save_and_exit)
-
-        self.fill_feed_list()
-
-    def save_and_exit(self):
-        """
-            Saves feeds to file and exits the app
-        """
-        self.write_feed_file()
-        self.app.exit(0)
-
-    def cell_changed(self, row, column):
-
-        # url changed
-        if column == 0:
-            self.feed_list[row]["url"] = (
-                unicode(self.table.item(row, 0).text())
-            )
-        # title changed
-        if column == 1:
-            title = unicode(self.table.item(row, 1).text())
-            if len(title) == 0:
-                self.table.setItem(row, 1, QTableWidgetItem("- None -"))
-            self.feed_list[row]["custom_title"] = None
-
-    def checkbox_clicked(self, row):
-
-        box = self.table.cellWidget(row, 2)
-
-        state = box.checkState()
-
-        if state == 0:
-            self.feed_list[row]["hidden"] = False
+    def title_edited(self, widget, path, text):
+        if len(text) > 0:
+            self.feeds[path][1] = text
         else:
-            self.feed_list[row]["hidden"] = True
+            self.feeds[path][1] = None
 
-    def center(self):
-        """
-            Centers the window
-        """
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
+    def hidden_toggled(self, widget, path):
+        self.feeds[path][2] = not self.feeds[path][2]
 
-    def remove_feed(self, row_number):
-        """
-            removes the entry from the selected line
-        """
-        del self.feed_list[row_number]
+    def remove_feed(self):
+        selection = self.treeview.get_selection()
+        result = selection.get_selected()
+        if result:
+            model, iter = result
+        model.remove(iter)
 
-        self.fill_feed_list()
-
-    def add_feed(self):
-        self.feed_list.append(
-            {
-                "url": "",
-                "custom_title": None,
-                "hidden": False
-            }
-        )
-
-        self.fill_feed_list()
-
-    def fill_feed_list(self):
-        """
-            Takes a list of dicts as load_feed_file creates them
-            and fills the table widget with the content
-        """
-
-        self.table.blockSignals(True)
-        self.table.setRowCount(0)
-        # resize table
-        self.table.setRowCount(len(self.feed_list))
-
-        for i, feed in enumerate(self.feed_list):
-            self.table.setItem(i, 0, QTableWidgetItem(feed["url"]))
-            # if no custom title is set, display - None -
-            if feed["custom_title"] is None:
-                title = "- None -"
-            else:
-                title = feed["custom_title"]
-            self.table.setItem(i, 1, QTableWidgetItem(title))
-
-            # set hide checkbox
-            box = QCheckBox()
-            if feed["hidden"]:
-                box.setChecked(True)
-            self.table.setCellWidget(i, 2, box)
-            box.stateChanged.connect(partial(self.checkbox_clicked, i))
-
-            button = QPushButton("delete")
-            button.clicked.connect(partial(self.remove_feed, i))
-            self.table.setCellWidget(i, 3, button)
-
-        self.table.blockSignals(False)
+    def new_feed(self):
+        self.feeds.append("URL", "", False)
 
     def write_feed_file(self):
         """
-            Writes the feed_list to the file
+            Writes the feeds list to the file
         """
 
         with open(self.feed_file, "w") as f:
-            for feed in self.feed_list:
-                comment = "#" if feed["hidden"] else ''
+            for feed in [r[0:3] for r in self.feeds]:
+                comment = "#" if feed[2] else ''
                 title = ''
-                if not feed["custom_title"] is None:
-                    title = " %s" % feed["custom_title"]
-                f.write("%s%s%s\n" % (comment, feed["url"], title))
+                if not feed[1] is None:
+                    title = " %s" % feed[1]
+                f.write("%s%s%s\n" % (comment, feed[0], title))
 
+    def load_feed_file(self, filename):
+        """
+            Reads content of the feed file and returns a GTK.ListStore
+        """
+        content = Gtk.ListStore(str, str, bool)
+        with open(filename, "r") as f:
+            for line in f:
+                try:
+                    if line[0] == "#":
+                        # cut out the comment and define this item as hidden
+                        line = line[1:]
+                        hidden = True
+                    else:
+                        hidden = False
+                    temp = line.split()
+                    url = temp[0]
+                    custom_title = None
+                    if len(temp) > 1:
+                        custom_title = " ".join(temp[1:])
+                    content.append([url, custom_title, hidden])
+                except IndexError:
+                    # empty lines are ignored
+                    pass
 
-def load_feed_file(filename):
-    """
-        Reads content of the feed file and returns a list of
-        dicts {"url", "custom_title"}
-    """
-    content = []
-
-    with open(filename, "r") as f:
-        for line in f:
-            try:
-                if line[0] == "#":
-                    # cut out the comment and define this item as hidden
-                    line = line[1:]
-                    hidden = True
-                else:
-                    hidden = False
-                temp = line.split()
-                url = temp[0]
-                custom_title = None
-                if len(temp) > 1:
-                    custom_title = " ".join(temp[1:])
-                content.append(
-                    {
-                        "url": url,
-                        "custom_title": custom_title,
-                        "hidden": hidden
-                    }
-                )
-            except IndexError:
-                # empty lines are ignored
-                pass
-
-    return content
+        return content
 
 
 if __name__ == '__main__':
@@ -215,12 +107,7 @@ if __name__ == '__main__':
     # get feed file name
     feed_file_name = sys.argv[1]
 
-    app = QApplication([])
-
-    feeds = load_feed_file(feed_file_name)
-
-    app.window = MainWindow(feeds, app, filename=feed_file_name)
-
-    app.window.show()
-
-    sys.exit(app.exec_())
+    window = MainWindow(filename=feed_file_name)
+    window.connect("delete-event", Gtk.main_quit);
+    window.show_all()
+    Gtk.main()
