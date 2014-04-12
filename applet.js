836cbc5725c616f1e311a673cd4674fea8b96f50
Kausheel Kumar <kausheelkumar@gmail.com>
2014-04-12 10:03:32 +1000

    Bug fix: applet won't start due to a callback error.

--- applet.js
+++ applet.js
@@ -419,12 +419,16 @@ FeedApplet.prototype = {
                 "show_feed_image", "show_feed_image", this.update_params, null);
 
         this.settings.bindProperty(Settings.BindingDirection.IN,
-                "notifications_enabled", "notifications_enabled", null, null);
+                "notifications_enabled", "notifications_enabled", this.empty_function, null);
 
         this.settings.bindProperty(Settings.BindingDirection.BIDIRECTIONAL,
                 "url", "url_list_str", this.url_changed, null);
 
         this.url_changed();
+    },
+
+    empty_function: function() {
+
     },
 
     build_context_menu: function() {
